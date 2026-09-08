cfe86df8d8a6c769c935f8d39bbe43a55916b14e
Edwin Cardenas <eacardenase@gmail.com>
2026-09-07 20:26:36 -0500

    feat: basic routes

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+    res.send("Root");
+});
+
+app.get("/hello", (req, res) => {
     res.send("Hello, World!");
+});
+
+app.get("/movies/action", (req, res) => {
+    res.send("Yeeey, action movies!");
 });
 
 app.listen(8080, () => {
